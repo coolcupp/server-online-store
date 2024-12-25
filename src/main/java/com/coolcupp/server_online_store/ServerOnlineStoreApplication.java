b02d5fa3974f4d1ced44671e80009b132cc8a9f0
coolcupp <yarchuk-maks@mail.ru>
2024-12-25 14:18:58 +0300

    initial commit

---
+++ src/main/java/com/coolcupp/server_online_store/ServerOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.coolcupp.server_online_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coolcupp/server_online_store/ServerOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolcupp.server_online_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
